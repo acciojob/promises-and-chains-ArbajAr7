78bac9cc11db01f0dd65cbf7f231fad77ca5dc56
Arbaj Ansari <83424578+ArbajAr7@users.noreply.github.com>
2024-05-30 23:44:50 +0545

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+function validateInput() {
+	event.preventDefault();
+	let name = document.getElementById("input_name").value;
+	let age = parseInt(document.getElementById("input_age").value);
+	if(name == "" || age =="")
+	{
+		document.getElementById("p_id").style.display="block";
+	}else {
+		document.getElementById("p_id").style.display="none";
+		console.log("name: "+name+" "+"age: "+age);
+		console.log(typeof age);
+		new Promise((resolve,reject) => {
+			setTimeout(() => {
+				if(age > 18)
+				{
+					resolve();
+				}else {
+					reject();
+				}
+			},4000);
+		})
+		.then(() => {
+			alert("Welcome, "+name+". You can vote.");
+		})
+		.catch(() => {
+			alert("Oh sorry "+name+". You aren't old enough.")
+		});
+	}
+}
+
+
+document.getElementById("btn").addEventListener("click", validateInput);
